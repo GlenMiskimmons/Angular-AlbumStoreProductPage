d7dc14f67022eb24bccfba8b88b1e7a43076b852
GlenMiskimmons <glenmiskimmons@gmail.com>
2018-08-22 21:47:08 +0100

    AlbumDataService
    Pluralsight project - Step 3 Creating a Service to get Album Data

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+  private readonly _albumUrl: string = '../assets/album.json';
+
+  constructor(
+    private _http: Http
+  ) { }
+
+  public getAlbum(id: number): Observable<any> {
+    return this._http.get(this._albumUrl).map(
+      response => response.json()
+    )
+  }
+}
